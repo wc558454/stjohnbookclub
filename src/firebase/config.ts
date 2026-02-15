429e8d99fd935a95b0e89d20ef0f138e2f95a988
William Bekele <wc558454@gmail.com>
2026-02-15 16:07:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-411540902-b46d7",
+  "appId": "1:261448411893:web:7636459105ce6ceec7b3bc",
+  "apiKey": "AIzaSyAa_9qBmxDIy-qG3iMke8V6Ad--SYLRKtk",
+  "authDomain": "studio-411540902-b46d7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "261448411893"
+};
